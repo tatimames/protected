6ff1ae23c6f05bba05422ce5b227981e96e9b1de
Tati Mames <tatimames@gmail.com>
2025-01-04 15:01:30 +0200

    add docker compose yml

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 from collections import defaultdict
 import utils
 
 app = Flask(__name__)
+CORS(app)
 
 JSONPLACEHOLDER_URL = "https://jsonplaceholder.typicode.com/posts"
 
